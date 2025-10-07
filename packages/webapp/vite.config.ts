776ca20eff40420abeac577e571779755b72c756
Ricardo Delfino <ricardodelfino@proton.me>
2025-10-07 09:30:55 -0300

    fix(webapp): Add optimizeDeps to Vite config for monorepo

--- packages/webapp/vite.config.ts
+++ packages/webapp/vite.config.ts
@@ -2,14 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	// This configuration is essential for a monorepo setup.
-	ssr: {
-		// Tells Vite's SSR build to bundle the 'shared' package instead of treating it as external.
-		noExternal: ['shared']
-	},
-	optimizeDeps: {
-		// Forces Vite's pre-bundler to include the 'shared' package for client-side code.
-		include: ['shared']
-	}
+	plugins: [sveltekit()]
 });
