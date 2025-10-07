eb18d9ca9e106e74e7e6ef186cf852418d109837
Ricardo Delfino <ricardodelfino@proton.me>
2025-10-07 01:25:48 -0300

    fix(webapp): Configure Vite for monorepo support

--- packages/webapp/vite.config.ts
+++ packages/webapp/vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		fs: {
+			// Allow serving files from one level up to the project root, which is necessary for a monorepo.
+			allow: [path.resolve(__dirname, '..')]
+		}
+	}
 });
